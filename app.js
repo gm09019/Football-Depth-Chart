7e10e42349ca2f1493342d99dff4340f112f061d
gm09019 <gm09019@gmail.com>
2024-07-21 19:23:30 +0000

    Fix authorization and add OAuth2 initialization

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM fully loaded and parsed');
 
   const API_KEY = 'AIzaSyBQT0HSLG0Duc7iRvcDtv5PFAGXknTk-aY';
-  const CLIENT_ID = '897172538215-q7h3a6je890n0ctgd4ca6cg1uv6eha9g.apps.googleusercontent.com'; // Replace with your actual client ID
+  const CLIENT_ID = '897172538215-q7h3a6je890n0ctgd4ca6cg1uv6eha9g.apps.googleusercontent.com';
   const SHEET_ID = '1e0EMRqmzGXB9etrRNMW7luqSsxehVeliGaTR8i8ASFw';
 
   console.log('API_KEY:', API_KEY);
